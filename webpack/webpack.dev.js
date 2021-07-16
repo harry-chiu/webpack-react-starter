068798dde39ed8e8565172b64c232a3aa9cefc20
harry.chiu <harry.chiu@jkos.com>
2021-07-16 12:47:29 +0800

    refactor: edit comment

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -25,18 +25,18 @@ module.exports = merge(webpackCommon, {
   // Plugin 設定
   plugins: [
     // webpack 內建 Hot Reload 插件
-    // -> 開發時不需重新整理頁面即可看到最新畫面
+    // 開發時不需重新整理頁面即可看到最新畫面
     new HotModuleReplacementPlugin(),
 
     // webpack-html-plugin 插件
-    // -> 產生 html 至打包後的專案中
+    // 產生 html 至打包後的專案中
     new HTMLWebpackPlugin({
       inject: true,
       template: path.resolve(__dirname, '../public/index.html'),
     }),
 
     // circular-dependency-plugin 插件
-    // -> 檢查是否有重複 import 的問題
+    // 檢查是否有重複 import 的問題
     new CircularDependencyPlugin({
       exclude: /\.js|node_modules/,
       failOnError: false,
@@ -59,7 +59,7 @@ module.exports = merge(webpackCommon, {
   devtool: 'eval-source-map',
 
   // 性能相關
-  // -> 只顯示 Error 相關
+  // 只顯示 Error 相關
   performance: {
     hints: 'warning',
   },
